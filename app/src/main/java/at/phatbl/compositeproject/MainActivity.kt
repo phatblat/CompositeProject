e3e2f1b4a6db40d0c060df99a6eaacecf803cd5c
Ben Chatelain <ben@octop.ad>
2021-07-20 20:39:23 -0600

    ✨ Add retrofit example request

--- app/src/main/java/at/phatbl/compositeproject/MainActivity.kt
+++ app/src/main/java/at/phatbl/compositeproject/MainActivity.kt
@@ -2,10 +2,13 @@ package at.phatbl.compositeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import at.phatbl.module.GitHub
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repos = GitHub().fetchRepos()
     }
 }
--- module/src/main/java/at/phatbl/module/GitHubService.kt
+++ module/src/main/java/at/phatbl/module/GitHubService.kt
@@ -1,25 +1,52 @@
 package at.phatbl.module
 
-import retrofit2.Call
-import retrofit2.Retrofit
+import kotlinx.coroutines.*
+import retrofit2.*
+import retrofit2.adapter.java8.Java8CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-
 interface GitHubService {
     @GET("users/{user}/repos")
-    fun listRepos(@Path("user") user: String?): Call<List<Repo?>?>?
+    suspend fun listRepos(@Path("user") user: String?): List<Repo>
 }
 
 data class Repo(
     var name: String
 )
 
-class GitHub {
-    var retrofit = Retrofit.Builder()
-        .baseUrl("https://api.github.com/")
-        .build()
+class GitHub : Callback<List<Repo>> {
+    var repos: List<Repo>? = null
 
-    var service = retrofit.create(GitHubService::class.java)
-    var repos: Call<List<Repo?>?>? = service.listRepos("octocat")
+    fun fetchRepos(): List<Repo>? {
+        var retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(Java8CallAdapterFactory.create())
+            .build()
+
+        var service = retrofit.create(GitHubService::class.java)
+
+        runBlocking {
+            launch {
+                repos = service.listRepos("octocat")
+            }
+        }
+
+        return repos
+    }
+
+    override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+        if (response.isSuccessful()) {
+            val repoList = response.body()
+            repoList?.forEach { repo -> println(repo.name) }
+        } else {
+            System.out.println(response.errorBody())
+        }
+    }
+
+    override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+        t.printStackTrace()
+    }
 }
